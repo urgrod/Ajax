8d88757c24e7dee6e9b7e8a711733c686f384e96
urgrod <lecam.gurvan@gmail.com>
2018-03-27 14:50:06 +0200

    chat mis au bon emplacement
    Co-Authored-By: Sam-Nezou <sam-nezou@users.noreply.github.com>

--- php/constants.php
+++ php/constants.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * @Author: Thibault Napoléon <Imothep>
- * @Company: ISEN Yncréa Ouest
- * @Email: thibault.napoleon@isen-ouest.yncrea.fr
- * @Created Date: 22-Jan-2018 - 14:18:14
- * @Last Modified: 29-Jan-2018 - 21:25:26
- */
 
   // Database constants.
   define('DB_USER', 'cw_cir2');

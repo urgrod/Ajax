76d9eff415b48b3693f2fbcbbcfd038593ef9135
urgrod <lecam.gurvan@gmail.com>
2018-03-29 16:48:30 +0200

    chat_debug
    Co-Authored-By: sam-nezou <sam-nezou@users.noreply.github.com>

--- js/chat.js
+++ js/chat.js
@@ -1,32 +1,30 @@
 'use strict';
 
 var websocket;
-// var login='XxXD4RK4NG3LXxX';
-var login='tuladiboufi';
-
-
+var login='xX_D4rk4ng3l_Xx';
 
 createWebSocket();
 
-function createWebSocket(){
-  websocket=new WebSocket('ws://172.17.5.64:12345');
+function createWebSocket()
+{
+  websocket = new WebSocket('ws://chen-co.local:12345');
 
   $('#chat-send').unbind('click').click(sendMessage);
-
-  websocket.onmessage=function(message){
-
+  websocket.onmessage = function(message)
+  {
     var textArea;
-    textArea=$('#chat-room');
-    textArea.val(textArea.val()+message.data+'\n');
+
+    textArea = $('#chat-room');
+    textArea.val(textArea.val() + message.data + '\n');
+    textArea.scrollTop(textArea.prop('scrollHeight'));
   }
 }
 
 function sendMessage(event){
   var message;
-
   event.preventDefault();
-
   message=$('#chat-message').val();
+  websocket.send(login+': '+message);
+  message=$('#chat-message').val('');
 
-  websocket.send(login+':'+message);
 }
